914cd0dea5f0990d08b445f1ee365aa8bc7fa057
aveliz1999 <aveliz1999@gmail.com>
2021-02-01 13:33:38 -0500

    Add authenticated routes

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,8 @@ import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom
 import Login from "./components/login/Login";
 import {UserContext} from "./context/User";
 import React, {useContext} from "react";
+import Gallery from "./components/gallery/Gallery";
+import Home from "./components/home/Home";
 
 export default function AppRouter() {
     const [user] = useContext(UserContext);
@@ -10,9 +12,12 @@ export default function AppRouter() {
         <Switch>
             {
                 user ? <>
-                    {
-                        // TODO implement authenticated routing
-                    }
+                    <Route exact path="/home">
+                        <Home/>
+                    </Route>
+                    <Route path="/">
+                        <Redirect to="/home"/>
+                    </Route>
                 </> : <>
                     <Route exact path="/login">
                         <Login/>
