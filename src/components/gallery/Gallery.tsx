d3e3909ba2382982d977ceedb5fa296c0d95fe24
aveliz1999 <github@veliz99.com>
2021-03-05 21:33:38 -0500

    Add media view in gallery

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Gallery.module.css';
+import Media, {MediaType} from "../media/Media";
 
 type GalleryProps = {
-    urls: string[],
+    media: MediaType[],
     hasNext: boolean,
     fetchNewImages: () => void
 }
 
 export default function Gallery(props: GalleryProps) {
+    const [currentMedia, setCurrentMedia] = useState<MediaType | undefined>();
+    const [currentScroll, setCurrentScroll] = useState(0);
+
+    useEffect(() => {
+        if(!currentMedia) {
+            window.scroll(0, currentScroll);
+        }
+    }, [currentMedia]);
+
     return <div>
-        <div className={styles.gallery}>
-            {
-                props.urls.map(url => <img className={styles.image} key={url} src={url}/>)
-            }
-        </div>
+        <Media media={currentMedia} onExit={() => {
+            setCurrentMedia(undefined);
+        }}/>
         {
-            props.hasNext && <button className={styles.moreButton} onClick={props.fetchNewImages}>More...</button>
+            !currentMedia && <>
+                <div className={styles.gallery}>
+                    {
+                        props.media.map(media => <img className={styles.image} key={media.id} src={`/api/media/${media.hash}/true.png`} onClick={() => {
+                            setCurrentScroll(window.scrollY);
+                            setCurrentMedia(media);
+                        }}/>)
+                    }
+                </div>
+                {
+                    props.hasNext && <button className={styles.moreButton} onClick={props.fetchNewImages}>More...</button>
+                }
+            </>
         }
     </div>
 }
